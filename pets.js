cd3fa8c861969ab09f0be5290e98653c08587bb0
jubaeddddd <jubaed.techie@gmail.com>
2025-05-27 16:43:30 +0600

    functional button to get desired category wise pets

--- pets.js
+++ pets.js
@@ -1,22 +1,50 @@
+//remove all active button
+const removeActive=()=>{
+   const allButtons=document.getElementsByClassName("buttons")
+   for(const button of allButtons){
+    button.classList.remove("active")
+   }
+}
+
+//load videos based on category
+const displayPetsBasedOnCategory = (pet_name) => {
+    fetch(`https://openapi.programming-hero.com/api/peddy/category/${pet_name}`)
+        .then(res => res.json())
+        .then(display => {
+            const activeBtn = document.getElementById(`btn-${pet_name}`)
+            removeActive();
+            activeBtn.classList.add("active")
+            displayPets(display.data)
+        })
+}
+
+
+
+
+
+
+
+
 //load categories
-const loadCategories=async()=>{
-    const fetched=await fetch('https://openapi.programming-hero.com/api/peddy/categories')
-    const res =await fetched.json();
-    const display=displayCategories(res.categories)
+const loadCategories = async () => {
+    const fetched = await fetch('https://openapi.programming-hero.com/api/peddy/categories')
+    const res = await fetched.json();
+    const display = displayCategories(res.categories)
 }
 //load pets
-const loadPets=async()=>{
-    const fetched=await fetch('https://openapi.programming-hero.com/api/peddy/pets')
-    const res=await fetched.json();
-    const display=displayPets(res.pets)
+const loadPets = async () => {
+    const fetched = await fetch('https://openapi.programming-hero.com/api/peddy/pets')
+    const res = await fetched.json();
+    const display = displayPets(res.pets)
 }
 //display pets
-const displayPets=(pets)=>{
-    const petSection=document.getElementById("pet-div")
-    for(const pet of pets){
-         const div=document.createElement('div')
-         div.classList="card bg-base-100 shadow-sm"
-         div.innerHTML=`
+const displayPets = (pets) => {
+    const petSection = document.getElementById("pet-div")
+    petSection.innerHTML = ""
+    for (const pet of pets) {
+        const div = document.createElement('div')
+        div.classList = "card bg-base-100 shadow-sm"
+        div.innerHTML = `
              <figure class="px-10 pt-10">
                     <img src="${pet.image}" />
              </figure>
@@ -24,19 +52,19 @@ const displayPets=(pets)=>{
                     <h1 class="font-bold text-2xl">${pet.pet_name}</h1>
                     <div class="flex gap-1">
                         <img class="h-5 w-5" src="${'https://img.icons8.com/?size=48&id=115221&format=png'}"/> 
-                        ${!pet.breed||pet.breed.length==0?'<h1 class="text-gray-600">Breed: Not Found</h1>':`<h1 class="text-gray-600">Breed: ${pet.breed}</h1>`}
+                        ${!pet.breed || pet.breed.length == 0 ? '<h1 class="text-gray-600">Breed: Not Found</h1>' : `<h1 class="text-gray-600">Breed: ${pet.breed}</h1>`}
                     </div>
                     <div class="flex gap-1">
                         <img class="h-5 w-5" src="${'https://img.icons8.com/?size=96&id=5VOqBjvi7siv&format=png'}"/> 
-                        ${!pet.date_of_birth||pet.date_of_birth.length==0?'<h1 class="text-gray-600">Birth: Not Found</h1>':`<h1 class="text-gray-600">Birth: ${pet.date_of_birth}</h1>`}
+                        ${!pet.date_of_birth || pet.date_of_birth.length == 0 ? '<h1 class="text-gray-600">Birth: Not Found</h1>' : `<h1 class="text-gray-600">Birth: ${pet.date_of_birth}</h1>`}
                     </div>
                     <div class="flex gap-1">
                         <img class="h-5 w-5" src="${'https://img.icons8.com/?size=100&id=1665&format=png'}"/>
-                         ${!pet.gender||pet.gender.length==0?'<h1 class="text-gray-600">Gender: Not Found</h1>':`<h1 class="text-gray-600">Gender: ${pet.gender}</h1>`} 
+                         ${!pet.gender || pet.gender.length == 0 ? '<h1 class="text-gray-600">Gender: Not Found</h1>' : `<h1 class="text-gray-600">Gender: ${pet.gender}</h1>`} 
                     </div>
                     <div class="flex gap-1">
                         <img class="h-5 w-5" src="${'https://img.icons8.com/?size=48&id=85843&format=png'}"/>
-                        ${!pet.price||pet.price.length==0?'<h1 class="text-gray-600">Price: Not Found</h1>':`<h1 class="text-gray-600">Price: ${pet.price}</h1>`}  
+                        ${!pet.price || pet.price.length == 0 ? '<h1 class="text-gray-600">Price: Not Found</h1>' : `<h1 class="text-gray-600">Price: ${pet.price}</h1>`}  
                     </div>
 
 
@@ -49,16 +77,16 @@ const displayPets=(pets)=>{
                     </div>
              </div>
          `
-         petSection.appendChild(div)
+        petSection.appendChild(div)
     }
 }
 // display categories
-const displayCategories=async(items)=>{
-    const categoriesButtons=document.getElementById('category-buttons')
-    for(const item of items){
-        const div=document.createElement('div')
-        div.innerHTML=`
-          <button class="btn w-32 py-7 px-9 font-bold text-xl"><img class="h-10" src="${item.category_icon}"/> ${item.category}</button>
+const displayCategories = async (items) => {
+    const categoriesButtons = document.getElementById('category-buttons')
+    for (const item of items) {
+        const div = document.createElement('div')
+        div.innerHTML = `
+          <button id="btn-${item.category}" onclick="displayPetsBasedOnCategory('${item.category}')" class="buttons btn w-32 py-7 px-9 font-bold text-xl hover:bg-[#cde6e7] hover:border-[#0E7A81] hover:rounded-full"><img class="h-10" src="${item.category_icon}"/> ${item.category}</button>
         `
         categoriesButtons.appendChild(div)
     }
